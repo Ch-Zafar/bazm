fb48d09ae3c23cb566c311298f4586bcb2b47ac3
Ch-Zafar <gzafar4879@gmail.com>
2025-08-16 01:13:25 +0500

    jaldi krle bhai

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../pages/Home';
 
 const ActionButton = () => {
+    const [isActive,setIsactive]=useContext(Context);
+
+    const onClick=()=>{
+      setIsactive(true)
+    }
+  
   return (
-    <button className='w-[300px] h-[80px] bg-black text-white rounded-3xl text-2xl cursor-pointer hover:scale-103 transition-all font-Orbitron'>
+    <button onClick={onClick}  className='w-[300px] h-[80px] bg-black text-white rounded-3xl text-2xl cursor-pointer hover:scale-103 transition-all font-Orbitron'>
         Begin your journey !
     </button>
   )
