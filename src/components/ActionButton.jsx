e302f3a809bcf0d171e2de2eb6f5b1a13f21f115
Zafar <gzafar4879@gmail.com>
2025-08-18 10:09:40 -0700

    inshallah

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../pages/Home'
 
-const ActionButton = () => {
+const ActionButton = ({visibility}) => {
+  const [isActive,setIsactive]=useContext(Context)
   return (
-    <button className='w-[300px] h-[80px] bg-black text-white rounded-3xl text-2xl cursor-pointer hover:scale-103 transition-all font-Orbitron'>
+    <button onClick={()=>{
+      setIsactive(true)
+    }} className='w-[300px] h-[80px] bg-black text-white rounded-3xl text-2xl cursor-pointer hover:scale-103 transition-all font-Orbitron'>
         Begin your journey !
     </button>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react'
+import React, { createContext, useState } from 'react'
 import Nav from '../components/Nav'
 import HeroImg from "/public/WhatsApp Image 2025-08-06 at 07.33.26_abdd3581.jpg"
 import ActionButton from '../components/ActionButton'
+
+
+export const Context=createContext()
+
 const Home = () => {
-  const [isActive,setIsactive]=useState('false')
+  const [isActive, setIsactive] = useState(false)
+  console.log(isActive)
+  
+
   return (
     <div className=' w-full h-screen   flex justify-center'>
       <Nav />
@@ -17,7 +24,7 @@ const Home = () => {
           <video
             className='w-full h-full block object-cover'
             src='/WhatsApp Video 2025-08-13 at 10.19.00_dc3d8428.mp4'
-            autoPlay={isActive===true}
+            autoPlay={isActive === true}
             loop
             muted
             playsInline
@@ -28,14 +35,22 @@ const Home = () => {
           <img src="/WhatsApp Image 2025-08-13 at 07.06.13_af497365.jpg" alt="" className='w-full h-full' />
         </div>
         {/* //////////for transparent white overlay/////////// */}
-        <div className="absolute inset-0 bg-white/80 flex flex-col justify-center items-center">
+        {
+          isActive===true?
+          (<div className="absolute inset-0 bg-white/80 flex flex-col justify-center items-center">
             <img src="/logo.png" alt="" className='w-[500px] h-[500px]' />
-            <ActionButton/>
-            
-        
-        
-        
-        </div>
+            <Context.Provider value={[isActive,setIsactive]}>
+              <ActionButton  />
+
+
+            </Context.Provider>
+
+
+
+
+          </div>)
+          :null
+        }
 
       </div>
 
