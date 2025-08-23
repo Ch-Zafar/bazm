db0b51dfa31d0dd43eba00bbf87f9a46116f60f8
Ch-Zafar <gzafar4879@gmail.com>
2025-08-23 16:29:29 +0500

    to PC

--- src/canvas/CanvasModel.jsx
+++ src/canvas/CanvasModel.jsx
@@ -1,25 +1,68 @@
-import React from "react"
+import React, { createContext, useState } from "react"
 import { Canvas } from "@react-three/fiber";
 import { Center, Environment, OrbitControls } from "@react-three/drei";
 import Model from "./Model";
+import { Decal } from '@react-three/drei';
+import { useTexture } from "@react-three/drei";
 
 
-
+export const colorContext = createContext();
 const CanvasModel = () => {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} /> 
-      <pointLight position={[0, 0, 5]} />
-      <mesh>
-        <Model scale={10}/>
-      </mesh>
-    <OrbitControls
-      enableZoom={true}   // allow zoom
-    enablePan={true}    // allow panning
-    enableRotate={true}
-    />
 
-    </Canvas>
+
+  const [color, setColor] = useState('white');
+  const handleColor = (color) => {
+    setColor(color)
+    localStorage.setItem("color", color)
+  }
+  return (
+    <>
+
+      <colorContext.Provider value={[color, setColor]}>
+        <Canvas>
+          <ambientLight intensity={0.5} />
+          {/* /////////////////for front ///////////////// */}
+          <directionalLight position={[5, 5, 5]} intensity={5} target-position={[0, 0, 0]} />
+          {/* ////////////////for back ///////////////////// */}
+          {/* <directionalLight position={[5,5,-10]} intensity={5} target-position={[0,0,0]}/> */}
+          <pointLight position={[0, 0, 5]} />
+          <mesh>
+            <Model scale={10} />
+            
+          </mesh>
+          <OrbitControls
+            enableZoom={true}   // allow zoom
+            enablePan={true}    // allow panning
+            enableRotate={true}
+          />
+
+        </Canvas>
+        {/* <input type="color" name="color" id="" onChange={handleColor} /> */}
+        <div
+          className="w-30 h-auto flex justify-between items-center"
+        >
+          <button
+            className="w-10 h-10 rounded-full bg-black border-0 border-black cursor-pointer hover:border-1 active:border-1" onClick={() => handleColor('black')}
+          >
+
+          </button>
+          <button
+            className="w-10 h-10 rounded-full bg-[#f5f5dc] border-0 border-black cursor-pointer hover:border-1 active:border-1" onClick={() => handleColor("#f5f5dc")}
+          >
+
+          </button>
+        </div>
+
+
+
+
+
+
+
+      </colorContext.Provider>
+
+
+    </>
   )
 }
 
--- src/canvas/Model.jsx
+++ src/canvas/Model.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { MeshStandardMaterial } from "three";
- import { useGLTF } from "@react-three/drei";
+ import { useGLTF, useTexture } from "@react-three/drei";
 
 
 
 const Model = (props) => {
+    const shirtColor=localStorage.getItem("color")
     const { scene } = useGLTF("/shirt_baked.glb");
-    scene.traverse((child) => {
+      const decalTexture = useTexture("/WhatsApp Image 2025-08-20 at 21.07.20_15601044.jpg")
+    
+    
+    useEffect(()=>{
+      scene.traverse((child) => {
     if (child.isMesh) {
-      child.material = new MeshStandardMaterial({ color: "red" });
+      child.material = new MeshStandardMaterial({ color: shirtColor,etalness: 0.5,
+  roughness: 0.5 });
     }
   });
+    },[shirtColor])
 
   return (
-    <primitive object={scene} {...props} />
+    <>
+      <primitive object={scene} {...props} />
+      
+    
+    
+    </>
 
   )
 }
