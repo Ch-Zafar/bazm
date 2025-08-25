c905f68953e68f75173cddc425e9a690915b09c6
Ch-Zafar <gzafar4879@gmail.com>
2025-08-25 23:01:17 +0500

    anither day

--- src/canvas/Model.jsx
+++ src/canvas/Model.jsx
@@ -1,33 +1,97 @@
 import React, { useContext, useEffect } from 'react'
-import { MeshStandardMaterial } from "three";
- import { useGLTF, useTexture } from "@react-three/drei";
+import { MeshStandardMaterial, PositionalAudio } from 'three';
+ import {Decal, useGLTF, useTexture } from "@react-three/drei";
 
 
 
 const Model = (props) => {
-    const shirtColor=localStorage.getItem("color")
-    const { scene } = useGLTF("/shirt_baked.glb");
-      const decalTexture = useTexture("/WhatsApp Image 2025-08-20 at 21.07.20_15601044.jpg")
-    
-    
-    useEffect(()=>{
-      scene.traverse((child) => {
-    if (child.isMesh) {
-      child.material = new MeshStandardMaterial({ color: shirtColor,etalness: 0.5,
-  roughness: 0.5 });
-    }
-  });
-    },[shirtColor])
+    const {nodes,materials}=useGLTF("/shirt_baked.glb");
+    console.log(nodes)
 
-  return (
-    <>
-      <primitive object={scene} {...props} />
+    const shirtTexture=useTexture("/WhatsApp Image 2025-08-20 at 21.07.20_15601044.jpg");
+
+
+
+    return(
+      <>
+        <mesh
+          geometry={nodes.T_Shirt_male.geometry}
+          material={materials.lambert1}
+          castShadow
+          receiveShadow
+        
+        
+        />
+        <meshStandardMaterial color="white" />
+
+        <Decal
+          position={[0,0.1,0.15]}
+          rotation={[0,0,0]}
+          scale={0.2}
+          map={shirtTexture}
+          depthTest={true}
+          depthWrite={false}
+        
+        
+        />
+      
+      
+      
+      </>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   const shirtColor=localStorage.getItem("color")
+  //   const { scene } = useGLTF("/shirt_baked.glb");
+  //     const decalTexture = useTexture("/WhatsApp Image 2025-08-20 at 21.07.20_15601044.jpg")
+    
+    
+  //   useEffect(()=>{
+  //     scene.traverse((child) => {
+  //   if (child.isMesh) {
+  //     child.material = new MeshStandardMaterial({ color: shirtColor,etalness: 0.5,
+  // roughness: 0.5 });
+  //   }
+  // });
+  //   },[shirtColor])
+
+  // return (
+  //   <>
+  //     <primitive object={scene} {...props} />
       
     
     
-    </>
+  //   </>
 
-  )
+  // )
 }
 
 export default Model
